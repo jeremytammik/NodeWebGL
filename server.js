e56b0cdfc4d63a47c2aa84dcec005c2b385c9f40
Jeremy Tammik <jeremy.tammik@eur.autodesk.com>
2015-04-22 22:30:12 +0200

    removed colsole log messages and cleaned up

--- server.js
+++ server.js
@@ -7,31 +7,7 @@ app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {}); // leads to public/index.html
 
-//var bodyParser = require('body-parser')
-//app.use( bodyParser.json() ); // to support JSON-encoded bodies
-//app.use(express.json()); // to support JSON-encoded body
-//app.get('/api/v1', api.v1);
-
-//var getRawBody = require('raw-body');
-//var typer = require('media-typer');
-//
-//app.use(function (req, res, next) {
-//  getRawBody(req, {
-//    length: req.headers['content-length'],
-//    limit: '1mb',
-//    encoding: typer.parse(req.headers['content-type']).parameters.charset
-//  }, function (err, string) {
-//    if (err)
-//      return next(err)
-//    req.text = string
-//    next()
-//  });
-//});
-
 var bodyParser = require('body-parser');
-//app.use( bodyParser.json() ); // to support JSON-encoded bodies
-//app.use( bodyParser() ); // use the deprecated bodyParser middleware for all routes; generates warnings
-//app.use( bodyParser.urlencoded() ); // generates warnings
 app.use( bodyParser.urlencoded({ extended: true }) );
 
 var api = require('./routes/api');
