f4cd8d831c07bb4c24750eb462a846438115cf82
Jeremy Tammik <jeremy.tammik@eur.autodesk.com>
2015-04-22 20:57:32 +0200

    REST API v1 POST skeleton working

--- routes/api.js
+++ routes/api.js
@@ -1,3 +1,26 @@
+
 exports.v1 = function(req, res) {
-  res.send('v1: Hello World!');
+  //console.log(JSON.stringify(req.method));
+  console.log(req.method);
+  //console.log(Object.keys(req));
+
+  if (req.method == 'GET') {
+    res.send('API v1 GET: Hello World!');
+  }
+  else if (req.method == 'POST') {
+    console.log(req.body);
+    res.send('API v1 POST: ' + JSON.stringify(req.body));
+    //var jsonString = '';
+    //req.on('data', function (data) {
+    //  jsonString += data;
+    //  if(jsonString.length > 1e6) {
+    //    jsonString = '';
+    //    response.writeHead(413, {'Content-Type': 'text/plain'}).end();
+    //    request.connection.destroy();
+    //  }
+    //});
+    //req.on('end', function () {
+    //  console.log(JSON.parse(jsonString));
+    //});
+  }
 };
--- server.js
+++ server.js
@@ -1,12 +1,42 @@
 var express = require('express');
 var app = express();
-var api = require('./routes/api');
 
 app.set('port', (process.env.PORT || 5000));
+
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {}); // leads to public/index.html
+
+//var bodyParser = require('body-parser')
+//app.use( bodyParser.json() ); // to support JSON-encoded bodies
+//app.use(express.json()); // to support JSON-encoded body
+//app.get('/api/v1', api.v1);
+
+//var getRawBody = require('raw-body');
+//var typer = require('media-typer');
+//
+//app.use(function (req, res, next) {
+//  getRawBody(req, {
+//    length: req.headers['content-length'],
+//    limit: '1mb',
+//    encoding: typer.parse(req.headers['content-type']).parameters.charset
+//  }, function (err, string) {
+//    if (err)
+//      return next(err)
+//    req.text = string
+//    next()
+//  });
+//});
+
+var bodyParser = require('body-parser');
+//app.use( bodyParser.json() ); // to support JSON-encoded bodies
+//app.use( bodyParser() ); // use the deprecated bodyParser middleware for all routes; generates warnings
+//app.use( bodyParser.urlencoded() ); // generates warnings
+app.use( bodyParser.urlencoded({ extended: true }) );
+
+var api = require('./routes/api');
 app.get('/api/v1', api.v1);
+app.post('/api/v1', api.v1);
 
 app.listen(app.get('port'), function() {
   console.log("Node WebGL app is running at localhost:"
